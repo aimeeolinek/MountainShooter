417f433c4df62558cf9aa2c4c61e21a330e2a4d0
aimeeolinek <aimeezinha3fev2024@gmail.com>
2026-06-12 19:29:05 -0300

    - Adjusting the menu: background, sound and fonts. - Adjusting the const file

--- code/Const.py
+++ code/Const.py
@@ -1,5 +1,16 @@
-WIN_WIDTH = 800
-C_ORANGE = (255, 165, 0)
-MENU_OPTION = ["Start", "Exit"]
+
+# C
+C_ORANGE = (255, 128, 0)
 C_WHITE = (255, 255, 255)
 C_YELLOW = (255, 255, 0)
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
