04c9890cefe0779b1c6923048ac7d7aeeb7a1038
aimeeolinek <aimeezinha3fev2024@gmail.com>
2026-06-13 11:35:02 -0300

    - Initial adjustments to the classes Game and Level, and adjustments to the Const file.

--- code/Level.py
+++ code/Level.py
@@ -2,6 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import pygame
+from code.Entity import Entity
 
 class Level:
-    pass
+    def __init__(self, window, name, game_mode):
+        self.window = window
+        self.name = name
+        self.game_mode = game_mode 
+        self.entity_list: list[Entity] = []
+
+    def run(self):
+        pass
