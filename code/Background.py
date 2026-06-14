c75c85a4ff01f967706b0ea9585d817a7d237223
aimeeolinek <aimeezinha3fev2024@gmail.com>
2026-06-14 20:57:10 -0300

    - Implementation the parallax effect.

--- code/Background.py
+++ code/Background.py
@@ -1,5 +1,5 @@
 import pygame
-from code.Const import WIN_WIDTH
+from code.Const import ENTITY_SPEED, WIN_WIDTH
 from code.Entity import Entity
 
 class Background(Entity):
@@ -7,6 +7,6 @@ class Background(Entity):
         super().__init__(name, position)
     
     def move(self):
-        self.rect.centerx -= 1
+        self.rect.centerx -= ENTITY_SPEED[self.name]
         if self.rect.right <= 0:
             self.rect.left = WIN_WIDTH
