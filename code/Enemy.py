ba6c7f21123eb488e9d3f2e20a0d8e81930ca89e
aimeeolinek <aimeezinha3fev2024@gmail.com>
2026-06-18 19:49:23 -0300

    - Creating Players and Enemies - Moving Players and Enemies

--- code/Enemy.py
+++ code/Enemy.py
@@ -1,6 +1,11 @@
 import pygame
-from .Entity import Entity
+from code.Const import ENTITY_SPEED, WIN_WIDTH
+from code.Entity import Entity
 
 class Enemy(Entity):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
     def move(self):
-        self.rect.x -= 2  # movimento simples para esquerda
+        self.rect.centerx -= ENTITY_SPEED[self.name]
+        if self.rect.right <= 0:
+            self.rect.left = WIN_WIDTH
--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 from code.Player import Player
 from code.Enemy import Enemy
@@ -16,4 +17,10 @@ class EntityFactory:
                     list_bg.append(Background(name=f'Level1Bg{i}', position=(WIN_WIDTH,0)))
                 return list_bg
             case 'Player1':
-                return Player('Player1', (10, WIN_HEIGHT / 2))
+                return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
+            case 'Player2':
+                return Player('Player2', (10, WIN_HEIGHT / 2 + 30))
+            case 'Enemy1':
+                return Enemy('Enemy1', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
+            case 'Enemy2':
+                return Enemy('Enemy2', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
